05280632991d2d5a9a0328bccb951fc3a7c2939d
Raymw1 <rayan.rmw@gmail.com>
2021-10-07 18:09:45 -0300

    Main logic to join box done

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,33 @@
 import React, {Component} from 'react';
-
 import {View, Text, Image, TextInput, TouchableOpacity} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import api from '../../services/api';
 
 import styles from './styles';
 
 import logo from '../../assets/logo.png';
 
 export default class Main extends Component {
+  state = {
+    newBox: '',
+  };
+
+  async componentDidMount() {
+    const box = await AsyncStorage.getItem('@RocketBox:box');
+    if (box) {
+      this.props.navigation.navigate('Box');
+    }
+  }
+
+  handleSignIn = async () => {
+    const {data} = await api.post('/boxes', {
+      title: this.state.newBox,
+    });
+    await AsyncStorage.setItem('@RocketBox:box', data._id);
+    this.props.navigation.navigate('Box');
+  };
+
   render() {
     return (
       <View style={styles.container}>
@@ -18,8 +39,10 @@ export default class Main extends Component {
           autoCapitalize="none"
           autoCorrect={false}
           underlineColorAndroid="transparent"
+          value={this.state.newBox}
+          onChangeText={text => this.setState({newBox: text})}
         />
-        <TouchableOpacity onPress={() => {}} style={styles.button}>
+        <TouchableOpacity onPress={this.handleSignIn} style={styles.button}>
           <Text style={styles.buttonText}>Create</Text>
         </TouchableOpacity>
       </View>
