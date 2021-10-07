c3d506f1406e422ec54dc4ed4f7d64e3cac9d5a9
Raymw1 <rayan.rmw@gmail.com>
2021-10-07 16:53:13 -0300

    Main page done

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -10,7 +10,18 @@ export default class Main extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Image source={logo} />
+        <Image style={styles.logo} source={logo} />
+        <TextInput
+          style={styles.input}
+          placeholder="Create a box"
+          placeholderTextColor="#999"
+          autoCapitalize="none"
+          autoCorrect={false}
+          underlineColorAndroid="transparent"
+        />
+        <TouchableOpacity onPress={() => {}} style={styles.button}>
+          <Text style={styles.buttonText}>Create</Text>
+        </TouchableOpacity>
       </View>
     );
   }
